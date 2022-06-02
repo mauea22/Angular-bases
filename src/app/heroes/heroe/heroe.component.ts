d3210d06f5c93539343e9b2072fa94632251da1d
mauea22 <mauea22@hotmail.com>
2022-06-02 10:23:35 -0300

    one way data binding

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -10,13 +10,21 @@ import { Component } from "@angular/core";
 export class HeroeComponent {
 
     nombre: string = 'Iroman'
-    edad: number = 30
+    edad: number = 45
 
     get nombreCapitalizado(){
         return this.nombre.toUpperCase();
     }
 
     obtenerNombre(): string {
-        return `${ this.nombre} - ${ this.edad}`
+        return `${ this.nombre} - ${ this.edad}`;
+    }
+
+    cambiarNombre(): void {
+        this.nombre = 'Spiderman';
+    }
+
+    cambiarEdad(): void{
+        this.edad = 30;
     }
 }
