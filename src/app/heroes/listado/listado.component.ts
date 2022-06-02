a39b593bc4bbdb92145b6aa41be259bffe043669
mauea22 <mauea22@hotmail.com>
2022-06-02 11:23:27 -0300

    listado.component *ngFor

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['spiderman', 'Goku', 'Iroman', 'Thor'];
+  heroeBorrado: string = '';
+
+  borrarHeroe(): void{
+    this.heroeBorrado = this.heroes.pop()|| '';
+    
+  }
+
+  
+
+}
+
+
