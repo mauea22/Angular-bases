046374096e2b438ff1475f909db5d884d56e60e6
mauea22 <mauea22@hotmail.com>
2022-06-06 10:57:58 -0300

    @output del hijo al padre

--- src/app/dbz/agregar/agregar.component.ts
+++ src/app/dbz/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component,EventEmitter, Input, Output } from '@angular/core';
+
 import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
@@ -8,28 +9,26 @@ import { Personaje } from '../interfaces/dbz.interface';
 })
 export class AgregarComponent  {
 
-  @Input() personajes: Personaje[] = [];
-
-
   @Input() nuevo: Personaje = {
     nombre: '',
     poder: 0
   }
+
+  @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
 
   agregar(){
     if(this.nuevo.nombre.trim().length === 0){
       return;
     }
     
-    this.personajes.push(this.nuevo);
+    this.onNuevoPersonaje.emit( this.nuevo);
 
     //resetear los inputs 
     this.nuevo = {
       nombre: '',
       poder: 0
     }
-
-    console.log(this.personajes);
+    
   }
 }
 
--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -28,4 +28,8 @@ export class MainPageComponent  {
     poder: 0
   }
 
+  agregarNuevoPersonaje(argumento:Personaje){
+    this.personajes.push(argumento);
+  }
+
 }
